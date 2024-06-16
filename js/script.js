1dc3cec3c1f53bbc1968df483e87dc6e95822c51
João Victor <ftcparamim@yahoo.com.br>
2024-06-16 11:38:14 -0300

    Inicio da crição da página Index

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var radio = document.querySelector('manual-btn')
+var cont = 1
+
+setInterval(() => {
+    proximaImg()
+}, 5000)
+
+function proximaImg(){
+    cont++
+
+    if(cont > 3){
+        cont = 1
+    }
+
+    document.getElementById('radio'+cont).checked = true
+}
